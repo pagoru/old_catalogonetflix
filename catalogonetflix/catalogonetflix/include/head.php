02c61572f49b13524b19098702304b1baf83768f
pagoru <pagoru@gmail.com>
2015-10-22 05:51:35 +0200

    muy necesario

--- catalogonetflix/catalogonetflix/include/head.php
+++ catalogonetflix/catalogonetflix/include/head.php
@@ -1,16 +1,16 @@
 <head>
-	<title>Catálogo Netflix</title>
+	<title>Catálogo Netflix España</title>
 	<link href="<?php echo WEB;?>css/style.css" rel="stylesheet">
 	<link rel="shortcut icon" href="<?php echo WEB;?>assets/logos/netflix-icon.ico">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	
-	<meta property="og:title" content="Catlálogo Netflix" />
+	<meta property="og:title" content="Catlálogo Netflix España" />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="<?php echo WEB;?>" />
 	<meta property="og:image" content="<?php echo WEB?>assets/logos/catlogonetflix.png" />
 	<meta property="og:locale" content="es_ES" />
 	<meta property="og:site_name" content="Catálogo Netflix" />
-	<meta property="og:description" content="Web que muestra el Catálogo de Netflix España." />
+	<meta property="og:description" content="Web que muestra el Catálogo de Netflix España" />
 	
 	<meta property="twitter:title" content="CatalogoNetflix" />
 	<meta property="twitter:description" content="">
